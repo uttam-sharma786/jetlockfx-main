a00b270d452af10eaa5d99884682aadfac4ad277
uttam-sharma786 <uttamsharma.8560@gmail.com>
2025-05-23 13:02:43 +0530

    changes firebase file

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,22 +1,33 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAoLY2y0yPnELRweInRpsTskrg5FA1OUT4", // Your project's API Key
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "jetlockfx.firebaseapp.com", // Your Auth Domain
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "jetlockfx", // Your Project ID
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "jetlockfx.appspot.com", // Your Cloud Storage Bucket
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "75333179968", // Your Messaging Sender ID (Project Number)
-  // appId: import.meta.env.VITE_FIREBASE_APP_ID || "YOUR_APP_ID_FROM_FIREBASE_CONSOLE", // <<== Get this from Project Settings!
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-490328172" // Your Google Analytics Measurement ID
+  apiKey: "AIzaSyAoLY2y0yPnELRweInRpsTskrg5FA1OUT4",
+  authDomain: "jetlockfx.firebaseapp.com",
+  projectId: "jetlockfx",
+  storageBucket: "jetlockfx.firebasestorage.app",
+  messagingSenderId: "75333179968",
+  appId: "1:75333179968:web:546c4bcf25cddc615cdda5",
+  measurementId: "G-W66M6HE15S"
 };
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
-export { app, auth, db };
+export { app, auth, db,analytics };
